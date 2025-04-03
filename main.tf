4b227cfc73c818221fd8f9b3928f59c3b1e142d2
CBS\mbratt <mbratt@carbenefitsolutions.co.uk>
2025-04-03 13:51:10 +0100

    Add PreProdEnvironment module configuration to main.tf

--- main.tf
+++ main.tf
@@ -12,4 +12,17 @@ module "DevEnvironment" {
   branch_name             = "main"
   username                = local.username
 }
+
+module "PreProdEnvironment" {
+  source                  = "./module/env"
+  env_name                = "preprod"
+  location                = "eastus"
+  os_type                 = "Linux"
+  sku_name                = "B1"
+  resource_group_name     = local.resource_group_name
+  resource_group_location = local.resource_group_location
+  website_url             = "https://github.com/AlexGuyNichols/nodejs-docs-hello-world.git"
+  branch_name             = "main"
+  username                = local.username
+}
 #endregion
